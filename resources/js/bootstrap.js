dca164c29235313f9c2e058235e4014395e12723
Ikhsandi15 <sandiikhsandic@gmail.com>
2023-11-18 22:53:25 +0700

    pusher client

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -15,18 +15,57 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER ?? 'mt1',
+    wsHost: process.env.MIX_PUSHER_HOST ? process.env.MIX_PUSHER_HOST : `ws-${process.env.MIX_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: process.env.MIX_PUSHER_PORT ?? 80,
+    wssPort: process.env.MIX_PUSHER_PORT ?? 443,
+    forceTLS: (process.env.MIX_PUSHER_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+});
+
+// Fungsi untuk menyimpan pesan ke dalam localStorage
+function saveMessage(message) {
+    var messages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+    messages.push(message);
+    localStorage.setItem('chatMessages', JSON.stringify(messages));
+}
+
+// Fungsi untuk menampilkan pesan dari localStorage
+function displayMessages() {
+    var dataMessage = document.getElementById('data-message');
+    var messages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+
+    // Menampilkan pesan dari localStorage
+    messages.forEach(function (message) {
+        var newMessage = document.createElement('div');
+        newMessage.textContent = message;
+        dataMessage.appendChild(newMessage);
+    });
+}
+
+// Panggil fungsi displayMessages saat halaman dimuat
+displayMessages();
+
+window.Echo.channel('channel-chat').listen('ChatEvent', (event) => {
+    // console.log(event.message.message);
+    // Mendapatkan elemen dengan ID data-message
+    var dataMessage = document.getElementById('data-message');
+
+    // Membuat elemen baru untuk menyimpan pesan
+    var newMessage = document.createElement('div');
+    newMessage.textContent = event.message.message;
+    // Menambahkan elemen baru ke dalam div dengan ID data-message
+    dataMessage.appendChild(newMessage);
+    
+    // Menyimpan pesan baru ke dalam localStorage
+    saveMessage(event.message.message);
+});
+

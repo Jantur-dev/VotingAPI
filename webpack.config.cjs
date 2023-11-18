eac07a2eb37c1678742211be08d4ce34353b1e9f
Ikhsandi15 <sandiikhsandic@gmail.com>
2023-11-18 22:50:37 +0700

    add webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,14 @@
+const mix = require('laravel-mix');
+
+mix.webpackConfig({
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+});
---
+++ webpack.mix.cjs
@@ -0,0 +1,5 @@
+// Import module 'laravel-mix' menggunakan ESM syntax
+const mix = require('laravel-mix');
+
+// Menggunakan ESM syntax untuk mix.js
+mix.js('resources/js/bootstrap.js', 'public/js/app.js');

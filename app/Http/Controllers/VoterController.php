5e625dac0762a14ab3f63c174271399f5010131b
Ikhsandi15 <sandiikhsandic@gmail.com>
2023-11-24 01:30:38 +0700

    add cekVote() method

--- app/Http/Controllers/VoterController.php
+++ app/Http/Controllers/VoterController.php
@@ -21,10 +21,12 @@ class VoterController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function create(): Response {
+    public function create(): Response
+    {
         return Inertia::render("Auth/Register");
     }
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'nis' => 'required|string|max:5|min:5',
             'email' => 'required|string|email|'
@@ -35,32 +37,33 @@ class VoterController extends Controller
         //         'errors' => $validator->errors()
         //     ]);
         // }
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return response()->json([
                 'status' => false,
                 'msg' => $validator->errors()
             ], 422);
         }
-        
+
         $data_voter = Voter::where('nis', $request->nis)->first();
-        if(isset($data_voter)) {
-            if($data_voter->amount_otp != 0) {
+        if (isset($data_voter)) {
+            if ($data_voter->amount_otp != 0) {
                 $expiredAt = now()->setTimezone('Asia/Jakarta')->addMinutes(5);
                 $voter = Voter::where('nis', $request->nis)->updateOrCreate(
-                [],
-                [    
-                    'email' => $request->email,
-                    'otp' => strval(random_int(1000, 9999)),
-                    'otp_expired_at' => $expiredAt,
-                    'amount_otp' => $data_voter->amount_otp-1
-                ]);
-                
-                // $data = array('name' => $voter->name, 'otp' => $voter->otp, 'nis' => $voter->nis);
-                // Mail::send('mail', $data, function ($message) use ($voter) {
-                //     $message->to($voter->email)->subject('Verify Email from JanturVote');
-                //     $message->from('vote@gmail.com', 'jantur');
-                // });
-    
+                    [],
+                    [
+                        'email' => $request->email,
+                        'otp' => strval(random_int(1000, 9999)),
+                        'otp_expired_at' => $expiredAt,
+                        'amount_otp' => $data_voter->amount_otp - 1
+                    ]
+                );
+
+                $data = array('name' => $voter->name, 'otp' => $voter->otp, 'nis' => $voter->nis);
+                Mail::send('mail', $data, function ($message) use ($voter) {
+                    $message->to($voter->email)->subject('Verify Email from JanturVote');
+                    $message->from('vote@gmail.com', 'Jantur-Dev');
+                });
+
                 return response()->json([
                     'status' => true,
                     'msg' => 'Berhasil. Silahkan cek email untuk verifikasi',
@@ -71,7 +74,7 @@ class VoterController extends Controller
             }
             return response()->json([
                 'status' => false,
-                'msg' => 'Sudah melebihi request otp, gunakan otp terakhir, tetap gagal?. Mintalah kertas dari panitia untuk vote' 
+                'msg' => 'Sudah melebihi request otp, gunakan otp terakhir, tetap gagal?. Mintalah kertas dari panitia untuk vote'
             ], 429);
         }
 
@@ -81,12 +84,13 @@ class VoterController extends Controller
         ], 400);
     }
 
-    public function verify(Request $request, $nis) {
+    public function verify(Request $request, $nis)
+    {
         $validator = Validator::make($request->all(), [
             'otp' => 'required|max:4|min:4'
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return response()->json([
                 'status' => false,
                 'msg' => $validator->errors()
@@ -94,14 +98,14 @@ class VoterController extends Controller
         }
 
         $voter = Voter::where('nis', $nis)->first();
-        if(isset($voter)) {
-            if($voter->otp === $request->otp) {
-                if($voter->otp === $request->otp && $voter->otp_expired_at >= now()->timezone('Asia/Jakarta')) {
+        if (isset($voter)) {
+            if ($voter->otp === $request->otp) {
+                if ($voter->otp === $request->otp && $voter->otp_expired_at >= now()->timezone('Asia/Jakarta')) {
                     $voter->email_verified_at = now()->timezone('Asia/Jakarta');
                     $voter->amount_otp = 3;
                     $voter->active_status = '1';
                     $voter->update();
-                    
+
                     return response()->json([
                         'status' => true,
                         'msg' => 'OTP valid. Voter berhasil diverifikasi.'
@@ -125,12 +129,13 @@ class VoterController extends Controller
         ], 400);
     }
 
-    public function resendOtp(Request $request) {
+    public function resendOtp(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'nis' => 'string|max:5|min:5|string'
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return response()->json([
                 'status' => false,
                 'msg' => $validator->errors()
@@ -138,23 +143,24 @@ class VoterController extends Controller
         }
 
         $data_voter = Voter::where('nis', $request->nis)->whereNotNull('email')->first();
-        if(isset($data_voter)) {
-            if($data_voter->amount_otp != 0) {
+        if (isset($data_voter)) {
+            if ($data_voter->amount_otp != 0) {
                 $expiredAt = now()->setTimezone('Asia/Jakarta')->addMinutes(5);
                 $voter = Voter::where('nis', $request->nis)->updateOrCreate(
-                [],
-                [    
-                    'otp' => strval(random_int(1000, 9999)),
-                    'otp_expired_at' => $expiredAt,
-                    'amount_otp' => $data_voter->amount_otp-1
-                ]);
-                
-                // $data = array('name' => $voter->name, 'otp' => $voter->otp, 'nis' => $voter->nis);
-                // Mail::send('mail', $data, function ($message) use ($voter) {
-                //     $message->to($voter->email)->subject('Verify Email from JanturVote');
-                //     $message->from('vote@gmail.com', 'jantur');
-                // });
-    
+                    [],
+                    [
+                        'otp' => strval(random_int(1000, 9999)),
+                        'otp_expired_at' => $expiredAt,
+                        'amount_otp' => $data_voter->amount_otp - 1
+                    ]
+                );
+
+                $data = array('name' => $voter->name, 'otp' => $voter->otp, 'nis' => $voter->nis);
+                Mail::send('mail', $data, function ($message) use ($voter) {
+                    $message->to($voter->email)->subject('Verify Email from JanturVote');
+                    $message->from('vote@gmail.com', 'Jantur-Dev');
+                });
+
                 return response()->json([
                     'status' => true,
                     'msg' => 'Berhasil. Silahkan cek email untuk verifikasi',
@@ -175,21 +181,22 @@ class VoterController extends Controller
         ], 406);
     }
 
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email|string',
             'otp' => 'required|min:4|max:4|string'
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return response()->json([
                 'status' => false,
                 'msg' => $validator->errors()
-            ]);
+            ], 422);
         }
 
-        $voter = Voter::where('nis', $request->nis)->where('email', $request->email)->whereNotNull('email_verified_at')->first();
-        if(isset($voter)) {
+        $voter = Voter::where('otp', $request->otp)->where('email', $request->email)->whereNotNull('email_verified_at')->first();
+        if (isset($voter)) {
             $voter->active_status = '1';
             $voter->update();
             return response()->json([
@@ -204,10 +211,11 @@ class VoterController extends Controller
         ], 400);
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         $voter_otp = Voter::where('otp', $request->otp)->first();
         $voter = Voter::where('nis', $voter_otp->nis)->where('email', $voter_otp->email)->whereNotNull('email_verified_at')->first();
-        if(isset($voter) && ($voter->active_status == '1')) {
+        if (isset($voter) && ($voter->active_status == '1')) {
             $voter->active_status = '0';
             $voter->update();
             return response()->json([
@@ -222,18 +230,18 @@ class VoterController extends Controller
         ], 400);
     }
 
-    public function vote($otp, $nis)
+    public function vote(Request $request)
     {
-        $candidate = Candidate::where('nis', $nis)->first();
-        $voter = Voter::where('otp', $otp)->first();
-        if (isset($voter) && $voter->vote_status != 'SUDAH') {
+        $candidate = Candidate::where('nis', $request->candidate_nis)->first();
+        $voter = Voter::where('nis', $request->voter_nis)->first();
+        if ($voter->vote_status != 'SUDAH') {
             if (isset($candidate)) {
                 $voter->candidateNis = $candidate->nis;
                 $voter->candidate = $candidate->name;
                 $voter->vote_status = 'SUDAH';
                 $voter->update();
 
-                if ($candidate->nis == $nis) {
+                if ($candidate->nis == $request->candidate_nis) {
                     $candidate->votes += 1;
                     $candidate->update();
                 }
@@ -249,10 +257,42 @@ class VoterController extends Controller
                 'msg' => 'Gagal vote. Coba lagi'
             ], 400);
         }
-
         return response()->json([
             'status' => false,
             'msg' => 'Gagal vote. Kamu sudah vote, tidak bisa vote 2x'
-        ], 422);
+        ], 406);
+    }
+
+    public function cekVote(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nis' => 'string|min:5|max:5'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'msg' => $validator->errors()
+            ], 422);
+        }
+
+        $voter = Voter::where('nis', $request->nis)->first();
+        if (isset($voter)) {
+            if ($voter->vote_status == 'SUDAH') {
+                return response()->json([
+                    'status' => true,
+                    'msg' => 'Voter sudah vote'
+                ]);
+            } else if ($voter->vote_status == 'BELUM') {
+                return response()->json([
+                    'status' => false,
+                    'msg' => 'Voter belum vote'
+                ], 406);
+            }
+        }
+        return response()->json([
+            'status' => false,
+            'msg' => 'Voter tidak ditemukan'
+        ], 404);
     }
 }
